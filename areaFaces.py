74f427298ec06bdd23753f53954266f0e2787d90
sallecta <sallecta@inbox.ru>
2018-03-22 23:44:52 +0300

    Create areaFaces.py

---
+++ areaFaces.py
@@ -0,0 +1,8 @@
+#print(repr(round(Gui.Selection.getSelectionEx()[0].SubObjects[0].Area/1000000,2)))
+# area = 0
+# area = area + Gui.Selection.getSelectionEx()[0].SubObjects[0].Area/1000000
+##
+area = 0
+for face in Gui.Selection.getSelectionEx()[0].SubObjects:
+    area = area + face.Area
+print("area = " + repr(area/1000000) + "m2")
