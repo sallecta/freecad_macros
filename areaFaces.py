e5acae9219f7471a037a02f9a2a77c080a50b887
sallecta <sallecta@inbox.ru>
2018-03-23 00:05:18 +0300

    Update areaFaces.py
    Count faces are from multiple objects selected

--- areaFaces.py
+++ areaFaces.py
@@ -3,6 +3,7 @@
 # area = area + Gui.Selection.getSelectionEx()[0].SubObjects[0].Area/1000000
 ##
 area = 0
-for face in Gui.Selection.getSelectionEx()[0].SubObjects:
-    area = area + face.Area
-print("area = " + repr(area/1000000) + " m2")
+for selObj in Gui.Selection.getSelectionEx():
+    for face in selObj.SubObjects:
+        area = area + face.Area
+print("area = " + repr(area/1000000) + " m2"
